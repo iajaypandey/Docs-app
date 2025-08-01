5a153ca394fcddf132827d52215695f9363a608c
Ajay Pandey <ajaypandeyy1123@gmail.com>
2025-08-02 02:54:15 +0530

    Change in Background.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
+import Background from './components/Background'
 
 function App() {
   return (
     <div className='bg-zinc-800 w-full h-screen relative'>
-      <div className=' absolute top-[3%] w-full flex justify-center py-10 text-zinc-600 font-semibold text-xl'>Documents.</div>
-      <h1 className='text-[13vw] tracking-tight leading-none absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-900'>Docs.</h1>
+      <Background/>
     </div>
   )
 }
---
+++ src/components/Background.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Background() {
+  return (
+    <>
+        <div className=' absolute top-[3%] w-full flex justify-center py-10 text-zinc-600 font-semibold text-xl'>Documents.</div>
+        <h1 className='text-[13vw] tracking-tight leading-none absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-900'>Docs.</h1>
+    </>
+  )
+}
+
+export default Background
