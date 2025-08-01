b3eb7a33d0185d151dab0c35dd9fe4d1a84885ac
Ajay Pandey <ajaypandeyy1123@gmail.com>
2025-08-02 03:01:41 +0530

    Make Changes in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className='bg-zinc-800 w-full h-screen relative'>
       <Background/>
+      <div className='fixed top-0 left-0 z-[3] w-full h-full'></div>
     </div>
   )
 }
