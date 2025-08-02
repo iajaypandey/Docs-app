5e0b548b9ddb1299b0c9b0984d7ebf1cd0f25857
Ajay Pandey <ajaypandeyy1123@gmail.com>
2025-08-03 03:03:12 +0530

    adding Functionality like motions in Card,jsx and Foreground.jsx file

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,37 @@
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { MdClose } from "react-icons/md";
+import { motion } from "motion/react"
 
 
-function Card({data}) {
+function Card({data, reference}) {
   return (
-    <div className="relative w-60 h-72 bg-zinc-900/90 rounded-[45px] text-white py-10 px-8 overflow-hidden">
-        <FaRegFileAlt/>
-        <p className="leading-tight m-2 font-semibold text-sm">
+    <motion.div 
+        drag dragConstraints={reference}
+        whileDrag={{scale: 1.2}}
+        dragElastic={0.2}
+        dragTransition={{bounceStiffness: 100}}
+        transition={{ type: "spring" }} animate={{ scale: 1 }}
+        className="relative flex-shrink-0 w-60 h-72 bg-zinc-900/90 rounded-[45px] text-white py-10 px-8 overflow-hidden">
+            <FaRegFileAlt/>
+            <p className="leading-tight m-2 font-semibold text-sm">
             {data.desc}
-        </p>
+            </p>
+
         <div className="footer absolute w-full bottom-0 left-0">
             <div className="flex justify-between items-center mb-2 py-3 px-8">
                 <h5>{data.fileSize}</h5>
                 <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
-                    <LuDownload size=".7rem" color="#fff"/>
+                    {data.close ? <MdClose /> : <LuDownload size=".7rem" color="#fff"/>}
                 </span>
             </div>
-            <div className="tag bg-green-600 w-full py-4 flex justify-center items-center">
-                <h3 className="text-sm font-semibold">Download Now</h3>
-            </div>
+            {data.tag.isOpen &&
+                (<div className={`tag w-full ${data.tag.tagColor === "sky" ? "bg-sky-600" : "bg-green-600"} py-4 flex justify-center items-center`}>
+                    <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+                </div>)
+            }
         </div>
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,24 +1,33 @@
+import { useRef } from 'react'
 import Card from './Card'
 
 function Foreground() {
+    const ref = useRef(null)
+
     const data = [
         {
             desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
             fileSize: ".7mb",
             close: true,
-            tag: {isOpen: true, tagtitle: "Download", tagColor: "green"},
+            tag: {isOpen: true, tagTitle: "Download Now", tagColor: "green"},
+        },
+        {
+            desc: "This is Dragable Doc web.",
+            fileSize: "14mb",
+            close: false,
+            tag: {isOpen: true, tagTitle: "loading", tagColor: "sky"},
         },
         {
             desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam unde nostrum maiores id nulla similique.",
-            fileSize: ".5mb",
-            close: true,
-            tag: {isOpen: true, tagtitle: "Download", tagColor: "green"},
+            fileSize: ".9mb",
+            close: false,
+            tag: {isOpen: true, tagTitle: "Download", tagColor: "red"},
         },
     ]
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full flex gap-3.5'>
+    <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-7 flex-wrap p-5'>
         {data.map((item)=>(
-            <Card data={item}/>
+            <Card data={item} reference={ref}/>
         ))}
     </div>
   )
